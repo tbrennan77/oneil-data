804dc04f41b4bf9422dcb963521447b7176e75b4
Tim Brennan <tbrennan@boondockwalker.com>
2015-04-07 10:24:11 -0400

    Onesuite template added

---
+++ page_onesuite.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Template Name: Industry Page Template
+ *
+ * This file adds the Industry template. This file assumes that nothing has been moved
+ * from the Genesis default.
+ *
+ * @category   Genesis_Sandbox
+ * @package    Templates
+ * @subpackage Page
+ * @author     Travis Smith
+ * @license    http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
+ * @link       http://wpsmith.net/
+ * @since      1.1.0
+ */
+
+/** Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) exit( 'Cheatin&#8217; uh?' );
+
+// Custom Filters and Actions
+add_filter('body_class', 'gs_add_landing_body_class' );
+add_filter('genesis_attr_entry-content', 'custom_add_css_attr' );
+add_action('wp_enqueue_scripts', 'custom_load_custom_style_sheet' );
+	
+/**
+ * Add page specific body class
+ *
+ * @param $classes array Body Classes
+ * @return $classes array Modified Body Classes
+ */
+function gs_add_landing_body_class( $classes ) {
+   $classes[] = 'landing';
+   return $classes;
+}
+
+function custom_add_css_attr( $attributes ) {
+ 
+ // add original plus extra CSS classes
+ $attributes['class'] .= ' industry toggled-on';
+ 
+ // return the attributes
+ return $attributes;
+ 
+}
+
+add_action('genesis_after_header', 'set_background_image');
+
+function set_background_image() {
+    if ( is_singular( 'post' ) || ( is_singular( 'page' ) && has_post_thumbnail() ) ) {
+            
+        //$image = array( 'src' => has_post_thumbnail() ? genesis_get_image( array( 'format' => 'url' ) ) : '' );
+		$image = genesis_get_image( array( 'format' => 'url' ));
+
+    } else {
+        // the fallback – our current active theme's default bg image
+        $image = get_background_image();
+    }
+
+    // And below, spit out the <style> tag... 
+	if ($image <> '') {
+	?>
+    <style type="text/css" id="custom-background-css-override">
+        .site-container {background: #fff url('<?php echo $image; ?>') no-repeat center 124px!important; }
+    </style>
+	<?php 
+	}
+}
+
+/**
+ * Add other page specific css
+ *
+ */
+function custom_load_custom_style_sheet() {
+	wp_enqueue_style('industry-stylesheet', CHILD_URL . '/css/industry.css', array(), PARENT_THEME_VERSION );
+}
+
+/** Force Layout */
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
+
+genesis();
